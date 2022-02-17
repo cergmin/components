3b03408d1978f493c9bb0c101fd9248168d04331
Sergey Minakov <cergmin@gmail.com>
2022-02-17 22:03:50 +1000

    Remove console.log from utilities/isCurrentPage.ts

--- docs/src/utilities/isCurrentPage.ts
+++ docs/src/utilities/isCurrentPage.ts
@@ -2,8 +2,6 @@ export default function isCurrentPage(
   targetPathname: string,
   currentPathname: string,
 ): boolean {
-  console.log(targetPathname);
-  console.log(currentPathname);
   const regexp = new RegExp(`^((${targetPathname}/)|(${targetPathname}$))`);
   return regexp.test(currentPathname);
 }
