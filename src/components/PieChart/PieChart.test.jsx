7ff1992122d2d7f08c895253425355299b2dd4ab
Sergey Minakov <cergmin@gmail.com>
2022-04-16 22:49:39 +1000

    Remove empty .test file

--- src/components/PieChart/PieChart.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import events from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
-import { PieChart } from '.';
-
-describe('PieChart', () => {});
